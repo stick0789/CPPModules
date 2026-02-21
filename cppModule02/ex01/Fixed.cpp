d99089e758b817f9de6d1a6b1e01d47bd2ad7f86
jaacosta <jaacosta@student.42barcelona.com>
2026-02-21 19:03:38 +0100

    ex 01 and 02 done and fixed

--- cppModule02/ex01/Fixed.cpp
+++ cppModule02/ex01/Fixed.cpp
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaacosta <jaacosta@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/21 17:19:58 by jaacosta          #+#    #+#             */
+/*   Updated: 2026/02/21 17:20:02 by jaacosta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "Fixed.hpp"
 
 Fixed::Fixed() : _fixPointValue(0)
@@ -8,12 +19,69 @@ Fixed::Fixed() : _fixPointValue(0)
 Fixed::Fixed(const Fixed &other)
 {
     std::cout << "Default Copy Constructor Called" << std::endl;
+    *this = other;
 }
 
 Fixed &Fixed::operator=(const Fixed &other)
 {
     std::cout << "Default Assigment Operator Called" << std::endl;
+    /* Verify we arn't assigment to ourself (a == a)*/
+    if (this != &other)
+    {
+        /* copy the bits from the other object*/
+        this->_fixPointValue = other.getRawBits();
+    }
+    /* Return the actual object to allow a = b = c*/
+    return *this;
 }
+
+/*Calling the get member function */
+int Fixed::getRawBits() const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_fixPointValue;
+}
+
+/*Calling the set member function*/
+void Fixed::setRawBits(int const raw)
+{
+    this->_fixPointValue = raw;
+}
+
+/*Calling the toFloat member function*/
+float Fixed::toFloat() const
+{
+    /* Divide for 2^8 to convert into a float without losing decimals*/
+    return (float)this->_fixPointValue / (1 << _numFractBits);
+}
+
+/*Calling the toInt member function*/
+int Fixed::toInt() const
+{
+    /* Move to the right to delete the 8 bits */
+    return (int)this->_fixPointValue >> _numFractBits;
+}
+
+
+std::ostream &operator<<(std::ostream &o, Fixed const &i) {
+    o << i.toFloat(); // Insert the float representation into the stream
+    return o;
+}
+
+Fixed::Fixed(const int _real)
+{
+    std::cout << "Int constructor called" << std::endl;
+    this->_fixPointValue = _real << _numFractBits;
+}
+
+Fixed::Fixed(const float _decimal)
+{
+    std::cout << "Float constructor called" << std::endl;
+    this->_fixPointValue = roundf(_decimal * (1 << _numFractBits));
+}
+
+
+
 
 Fixed::~Fixed()
 {
--- cppModule02/ex01/Fixed.hpp
+++ cppModule02/ex01/Fixed.hpp
@@ -1,5 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaacosta <jaacosta@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/21 17:20:12 by jaacosta          #+#    #+#             */
+/*   Updated: 2026/02/21 17:20:14 by jaacosta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <iostream>
 #include <string>
+#include <ostream>
 #include <cmath>
 
 class Fixed
@@ -13,8 +25,8 @@ class Fixed
         Fixed(const Fixed &other);/* Default Copy Constructor*/
         Fixed &operator=(const Fixed &other); /* Default Assigment Operator Constructor*/
         
-        Fixed(const int);/* Constructor that takes a constant integer as a parameter.*/
-        Fixed(const float);/* Constructor that takes a constant floating-point number as a parameter.*/
+        Fixed(const int _real);/* Constructor that takes a constant integer as a parameter.*/
+        Fixed(const float _decimal);/* Constructor that takes a constant floating-point number as a parameter.*/
 
         float toFloat( void ) const;/* Function that converts the fixed-point value to a floating-point value.*/
         int toInt( void ) const;/* Function that converts the fixed-point value to an integer value.*/
@@ -23,3 +35,5 @@ class Fixed
         void setRawBits( int const raw );/* Function that sets the raw value of the fixed-point number.*/
         
 };
+
+std::ostream &operator<<(std::ostream &o, Fixed const &i);
