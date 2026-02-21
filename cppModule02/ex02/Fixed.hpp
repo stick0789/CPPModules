c3fe77ad03f3364c56e96afc9341798e9dbc88b4
Jair Acosta <stick_789@live.com>
2026-02-21 06:51:04 -0500

    ex02 fixing and adding code

--- cppModule02/ex02/Fixed.hpp
+++ cppModule02/ex02/Fixed.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 class Fixed
 {
@@ -11,6 +12,12 @@ class Fixed
         ~Fixed();/* Default Destructor*/
         Fixed(const Fixed &other);/* Default Copy Constructor*/
         Fixed &operator=(const Fixed &other); /* Default Assigment Operator Constructor*/
+        
+        Fixed(const int);/* Constructor that takes a constant integer as a parameter.*/
+        Fixed(const float);/* Constructor that takes a constant floating-point number as a parameter.*/
+
+        float toFloat( void ) const;/* Function that converts the fixed-point value to a floating-point value.*/
+        int toInt( void ) const;/* Function that converts the fixed-point value to an integer value.*/
 
         int getRawBits( void ) const;/* Function that returns the raw value of the fixed-point value.*/
         void setRawBits( int const raw );/* Function that sets the raw value of the fixed-point number.*/
