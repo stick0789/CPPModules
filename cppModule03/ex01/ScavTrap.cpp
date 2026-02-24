f6478afa4c21d8404ec03b6cf9cc5e4ffbf987cc
Jair Acosta <stick_789@live.com>
2026-02-24 08:44:22 -0500

    ex01 ScavTrap part

--- cppModule03/ex01/ScavTrap.cpp
+++ cppModule03/ex01/ScavTrap.cpp
@@ -11,5 +11,39 @@
 /* ************************************************************************** */
 #include "ScavTrap.hpp"
 
+ScavTrap::ScavTrap(std::string _name) : ClapTrap(_name)
+{
+    /* using this, set the new points to the child scavtrap.*/
+    this->_hitPoints = 100;
+    this->_energyPoints = 50;
+    this-> _attackDamage = 20;
+    std::cout << "ScavTrap " << _name << " has been created!" << std::endl;
+}
 
+ScavTrap::~ScavTrap()
+{
+    std::cout << "ScavTrap " << _name << " has been destroyed!" << std::endl;
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap &other)
+{
+    if (this != &other) {
+        ClapTrap::operator=(other); // Llama al operador del padre
+    }
+    return *this;
+}
+
+void ScavTrap::attack(const std::string& target)
+{
+    if (_energyPoints > 0 && _hitPoints > 0)
+    {
+        _energyPoints--;
+        std::cout << "ScavTrap " << _name << " fiercely attacks " << target  << ", causing " << _attackDamage << " points of damage!" << std::endl;
+    }
+}
+
+void ScavTrap::guardGate()
+{
+    std::cout << "ScavTrap " << _name << " is now in Gate keeper mode." << std::endl;
+}
 
--- cppModule03/ex01/ScavTrap.hpp
+++ cppModule03/ex01/ScavTrap.hpp
@@ -9,10 +9,22 @@
 /*   Updated: 2026/02/23 20:19:00 by jaacosta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
 #include "ClapTrap.hpp"
 
-class ScavTrap : public ClapTrap
+class ScavTrap : public ClapTrap /*this mean ScavTrap will have all the functions of ClapTrap*/
 {
+    public:
+        ScavTrap();/*Constructor by defaultFOC*/
+        ScavTrap(std::string _name); /*Constructor required in the exercise*/
+        ScavTrap(const ScavTrap &other);/* Copy constructor FOC*/
+        ScavTrap &operator=(const ScavTrap &other);/*Copy Assigment Constructor FOC*/
+        ~ScavTrap();/*Destructor FOC*/
 
+        void attack(const std::string &target);/*replace the function of the father*/
+        void guardGate();/*function required*/
 };
 
+#endif
