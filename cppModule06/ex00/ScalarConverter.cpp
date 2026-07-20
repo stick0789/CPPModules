91c9321891ab8c7905777bf5d246f2a110d1aaec
jaacosta <jaacosta@student.42barcelona.com>
2026-07-20 21:45:29 +0200

    06..

--- cppModule06/ex00/ScalarConverter.cpp
+++ cppModule06/ex00/ScalarConverter.cpp
@@ -27,9 +27,20 @@ bool isChar(const std::string &param)
 	return (false);
 }
 
-bool isIntt(const std::string &param)
+bool isInt(const std::string &param)
 {
+    size_t i = 0;
 
+    if (param[i] == '+' || param[i] == '-')
+        i++;
+    if (i == param.length())
+        return false;
+    for (; i < param.length(); i++)
+    {
+        if (!std::isdigit(param[i]))
+            return false;
+    }
+    return true;
 }
 
 bool isFloat(const std::string &param)
@@ -42,13 +53,6 @@ bool isDouble(const std::string &param)
 
 }
 
-                /*
-            if (std::round(std::stof(param)) > 31 && std::round(std::stof(param)) < 127)
-				std::cout << "'" << static_cast<char>(std::round(std::stof(param))) << "'" << std::endl;
-			else if (std::round(std::stof(param)) >= CHAR_MIN && std::round(std::stof(param)) <= UCHAR_MAX)
-				std::cout << "Non displayable" << std::endl;
-			else
-				throw Input(); */
 void tochar(const std::string &param, input type)
 {
     std::cout << "Char: ";
@@ -79,9 +83,40 @@ void tochar(const std::string &param, input type)
     }
 }
 
-toInt
-toFloat
-toDouble
+void toInt(const std::string &param, input type)
+{
+    std::cout << "int: ";
+
+    if (type == CHAR)
+    {
+        char c = (param.length() == 3 && param[0] == '\'') ? param[1] : param[0];
+        std::cout << static_cast<int>(c) << std::endl;
+        return;
+    }
+
+    // Checks the excepcional cases
+    if (param == "nan" || param == "nanf" || param == "inf" || 
+        param == "+inf" || param == "-inf" || param == "inff" || 
+        param == "+inff" || param == "-inff")
+    {
+        std::cout << "impossible" << std::endl;
+        return;
+    }
+
+    double val = std::strtod(param.c_str(), NULL);
+    // Verify the overflow
+    if (val <= INT_MAX && val >= INT_MIN)
+    {
+        std::cout << static_cast<int>(val) << std::endl;
+    }
+    else
+    {
+        std::cout << "impossible" << std::endl;
+    }
+}
+
+//toFloat
+//toDouble
 
 void ScalarConverter::convert(const std::string &param)
 {
